06d35c79901b710619e174749a29bda09961c5c1
fredybintang <fredybintangtg@gmail.com>
2024-02-01 14:07:05 +0700

    Add login register ui

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,16 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container">
         <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <a class="navbar-brand" href="#">LOGO</a>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link {{ Route::is('index') ? 'active' : '' }}" aria-current="page" href="{{ route('index') }}">Beranda</a>
+                        <a class="nav-link {{ Route::is('index') ? 'active' : '' }}" aria-current="page"
+                            href="{{ route('index') }}">Beranda</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
@@ -60,7 +60,8 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ Route::is('news') ? 'active' : '' }}" href="{{ route('news') }}">Berita</a>
+                        <a class="nav-link {{ Route::is('news') ? 'active' : '' }}"
+                            href="{{ route('news') }}">Berita</a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
@@ -83,17 +84,18 @@
                         </ul>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link {{ Route::is('contact-us') ? 'active' : '' }}" href="{{ route('contact-us') }}">Kontak Kami</a>
+                        <a class="nav-link {{ Route::is('contact-us') ? 'active' : '' }}"
+                            href="{{ route('contact-us') }}">Kontak Kami</a>
                     </li>
                 </ul>
-                <!-- <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">register</a>
-              </li>
-            </ul> -->
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">register</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - Dharma Wanita</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"
+        integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    {{-- @include('components.navbar') --}}
+
+    <main class="auth">
+        <div class="auth-content p-3">
+            <img src="{{ asset('assets/circle-left.png') }}" class="circle-left" alt="circle-left">
+            <img src="{{ asset('assets/circle-right.png') }}" class="circle-right" alt="circle-right">
+            <img src="{{ asset('assets/ring-circle.png') }}" class="ring-circle" alt="ring-circle">
+            <div class="auth-card p-3 my-3">
+                <div class="card-header d-flex flex-column align-items-center mb-3 fs-3 fw-bold">
+                    <img src="{{ asset('assets/logo.png') }}" alt="logo" width="50" height="50">
+                    <div class="dwp text-center">Dharma Wanita Persatuan</div>
+                </div>
+                @yield('content')
+            </div>
+        </div>
+    </main>
+
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+
+</body>
+
+</html>
