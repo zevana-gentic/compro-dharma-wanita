4bd9550667108eea2265e48bf58f2fec0c0d9f41
Anando Rahul <anandorahul@gmail.com>
2024-11-20 21:37:30 +0700

    Add page title in dashboard sekda

--- app/Http/Controllers/Sekda/DashboardController.php
+++ app/Http/Controllers/Sekda/DashboardController.php
@@ -11,6 +11,7 @@ class DashboardController extends Controller
 {
     public function dashboard()
     {
+        $data['page_title'] = 'Dashboard Sekda';
         $data['members'] = User::where('role', '!=', 1)->latest()->limit(10)->get();
         $data['total_news'] = News::count();
         // $data['total_dwp_member'] = User::where('role', '!=', 1)->count();
