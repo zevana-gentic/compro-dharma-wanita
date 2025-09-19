02f4cb9d948330195b7a2bb988ba3f07af80f701
Anando Rahul <anandorahul@gmail.com>
2025-09-19 13:19:07 +0700

    Fix issues in gallery and news

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Auth;
 use File;
 use Storage;
 use Validator;
--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
+use Auth;
 use Storage;
 use Validator;
 use Str;
